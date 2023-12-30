b84865bbefbeda57ed5aca93e58c6b3f07501e04
THEjacob1000 <jacob.s.sanderson@gmail.com>
2023-12-31 09:46:24 +1100

    feat: added sound effects

--- src/slices/Hero/Shapes.jsx
+++ src/slices/Hero/Shapes.jsx
@@ -93,10 +93,17 @@ function Geometries() {
     }),
   ];
 
+  const soundEffects = [
+    new Audio("/sounds/knock1.ogg"),
+    new Audio("/sounds/knock2.ogg"),
+    new Audio("/sounds/knock3.ogg"),
+  ];
+
   return geometries.map(({ position, r, geometry }) => (
     <Geometry
       key={JSON.stringify(position)}
       position={position.map((p) => p * 2)}
+      soundEffects={soundEffects}
       geometry={geometry}
       materials={materials}
       r={r}
@@ -104,7 +111,13 @@ function Geometries() {
   ));
 }
 
-function Geometry({ r, position, geometry, materials }) {
+function Geometry({
+  r,
+  position,
+  geometry,
+  materials,
+  soundEffects,
+}) {
   const meshRef = useRef();
   const [visible, setVisible] = useState(false);
   const startingMaterial = getRandomMaterial();
@@ -113,6 +126,9 @@ function Geometry({ r, position, geometry, materials }) {
   }
   function handleClick(e) {
     const mesh = e.object;
+
+    gsap.utils.random(soundEffects).play();
+
     gsap.to(mesh.rotation, {
       x: `+=${gsap.utils.random(0, 2)}`,
       y: `+=${gsap.utils.random(0, 2)}`,

353710398c6b12d8d3b04015b5083984de0d7413
THEjacob1000 <jacob.s.sanderson@gmail.com>
2023-12-31 15:32:11 +1100

    feat: started navbar and about page

---
+++ src/slices/Biography/index.tsx
@@ -0,0 +1,25 @@
+import Bounded from "@/components/Bounded";
+import { Content } from "@prismicio/client";
+import { SliceComponentProps } from "@prismicio/react";
+
+/**
+ * Props for `Biography`.
+ */
+export type BiographyProps =
+  SliceComponentProps<Content.BiographySlice>;
+
+/**
+ * Component for "Biography" Slices.
+ */
+const Biography = ({ slice }: BiographyProps): JSX.Element => {
+  return (
+    <Bounded
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+    >
+      <div className="grid gap-x-8 gap-y-6 md:grid-cols-[2fr 1fr]"></div>
+    </Bounded>
+  );
+};
+
+export default Biography;

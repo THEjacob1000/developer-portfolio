6a4473301a7e896f6725f1d319ef04121531e202
THEjacob1000 <jacob.s.sanderson@gmail.com>
2024-01-01 08:55:36 +1100

    style: Adjusted background styling to add texture

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Urbanist } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import clsx from "clsx";
 
 const urbanist = Urbanist({ subsets: ["latin"] });
 
@@ -18,10 +19,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="bg-slate-900 text-slate-100">
-      <body className={urbanist.className}>
+      <body
+        className={clsx(urbanist.className, "relative min-h-screen")}
+      >
         <Header />
         {children}
         <Footer />
+        <div className="absolute inset-0 -z-50 max-h-screen background-gradient" />
+        <div className="absolute pointer-events-none inset-0 -z-40 h-full bg-[url('/noisetexture.jpg')] opacity-20 mix-blend-soft-light" />
       </body>
     </html>
   );

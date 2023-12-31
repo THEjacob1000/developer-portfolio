93510f204620ab7bd4e373569402eb1dce9e432b
THEjacob1000 <jacob.s.sanderson@gmail.com>
2024-01-01 08:24:24 +1100

    style: styled navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,27 +1,14 @@
 import { createClient } from "@/prismicio";
 import { PrismicNextLink } from "@prismicio/next";
 import Link from "next/link";
+import NavBar from "@/components/NavBar";
 
 const Header = async () => {
   const client = createClient();
   const settings = await client.getSingle("settings");
   return (
     <header className="top-0 z-50 mx-auto max-w-7xl md:stick md:top-4">
-      <nav>
-        <ul>
-          <li>
-            <Link href="/" aria-label="Home Page">
-              {settings.data.name}
-            </Link>
-          </li>
-          {settings.data.nav_item.map(({ link, label }, index) => (
-            <li key={index}>
-              <PrismicNextLink field={link}>{label}</PrismicNextLink>
-            </li>
-          ))}
-          <li></li>
-        </ul>
-      </nav>
+      <NavBar settings={settings} />
     </header>
   );
 };

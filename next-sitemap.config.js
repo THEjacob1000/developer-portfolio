c32842084323a70f8c62adb1ad61238853cc36a8
THEjacob1000 <jacob.s.sanderson@gmail.com>
2024-01-02 13:16:48 +1100

    build: Created sitemaps for SEO

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://www.jacobdevelops.com",
+  exclude: ["/slice-simulator"],
+  generateRobotsTxt: true, // (optional)
+  changefreq: "weekly",
+  sitemapBaseFileName: "sitemap",
+  outDir: "./public/sitemaps",
+};

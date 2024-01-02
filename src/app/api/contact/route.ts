123063b643ee9406816c644123c735f90ea4a831
THEjacob1000 <jacob.s.sanderson@gmail.com>
2024-01-02 11:51:28 +1100

    feat: added contact form functionality with resend

---
+++ src/app/api/contact/route.ts
@@ -0,0 +1,28 @@
+import { EmailTemplate } from "@/components/EmailTemplate";
+import { Resend } from "resend";
+import * as React from "react";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function POST(request: Request) {
+  try {
+    const req = await request.json();
+    const { data, error } = await resend.emails.send({
+      from: "Jacob's Portfolio <onboarding@resend.dev>",
+      to: ["jacob.s.sanderson@gmail.com"],
+      subject: "New Contact Form Submission",
+      react: EmailTemplate({
+        name: req.name,
+        email: req.email,
+        message: req.message,
+      }) as React.ReactElement,
+    });
+
+    if (error) {
+      return Response.json({ error });
+    }
+    return Response.json({ data });
+  } catch (error) {
+    return Response.json({ error });
+  }
+}
--- src/slices/ContactForm/index.tsx
+++ src/slices/ContactForm/index.tsx
@@ -4,6 +4,7 @@ import { SliceComponentProps } from "@prismicio/react";
 import Heading from "@/components/Heading";
 import Bounded from "@/components/Bounded";
 import { useState } from "react";
+import { FormEvent } from "react";
 
 /**
  * Props for `ContactForm`.
@@ -16,6 +17,30 @@ export type ContactFormProps =
  */
 const ContactForm = ({ slice }: ContactFormProps): JSX.Element => {
   const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const onSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+
+    try {
+      const res = await fetch("/api/contact", {
+        method: "POST",
+        body: JSON.stringify({
+          name,
+          email,
+          message,
+        }),
+        headers: {
+          "content-type": "application/json",
+        },
+      });
+      console.log(res);
+    } catch (error) {
+      console.error("Err", error);
+    }
+  };
+
   return (
     <Bounded
       data-slice-type={slice.slice_type}
@@ -23,10 +48,25 @@ const ContactForm = ({ slice }: ContactFormProps): JSX.Element => {
     >
       <div className="rounded-2xl border-2 border-slate-800 bg-slate-900 px-4 py-10 md:px-8 md:py-20">
         <Heading as="h1">{slice.primary.heading}</Heading>
-        <form>
-          <input type="text" placeholder="name" />
-          <input type="email" placeholder="email" />
-          <textarea placeholder="message" />
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            placeholder="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="email"
+            placeholder="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <textarea
+            placeholder="message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <button type="submit">Submit</button>
         </form>
       </div>
     </Bounded>

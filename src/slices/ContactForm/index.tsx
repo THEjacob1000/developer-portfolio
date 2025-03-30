bcc76036ee3bb2e8737b6606a58d1999001d7744
THEjacob1000 <jacob@jacobdevelops.com>
2025-03-30 11:46:58 +1100

    chore: add form components using react-hook-form and zod for validation, update ContactForm to utilize new components, and include new dependencies in package.json

--- src/slices/ContactForm/index.tsx
+++ src/slices/ContactForm/index.tsx
@@ -1,38 +1,53 @@
 "use client";
+import Bounded from "@/components/Bounded";
+import Heading from "@/components/Heading";
+import { zodResolver } from "@hookform/resolvers/zod";
 import type { Content } from "@prismicio/client";
 import type { SliceComponentProps } from "@prismicio/react";
-import Heading from "@/components/Heading";
-import Bounded from "@/components/Bounded";
-import { useState } from "react";
-import type { FormEvent } from "react";
-import { MdArrowOutward } from "react-icons/md";
 import { useRouter } from "next/navigation";
+import { createContext } from "react";
+import type { ControllerProps } from "react-hook-form";
+import {
+	Controller,
+	type FieldPath,
+	type FieldValues,
+	FormProvider as Form,
+	useForm,
+} from "react-hook-form";
+import { MdArrowOutward } from "react-icons/md";
 import { toast } from "sonner";
+import { z } from "zod";
 
 /**
  * Props for `ContactForm`.
  */
 export type ContactFormProps = SliceComponentProps<Content.ContactFormSlice>;
 
+const formSchema = z.object({
+	name: z.string().min(1),
+	email: z.string().email(),
+	message: z.string().min(1),
+});
+
 /**
  * Component for "ContactForm" Slices.
  */
 const ContactForm = ({ slice }: ContactFormProps) => {
-	const [name, setName] = useState("");
-	const [email, setEmail] = useState("");
-	const [message, setMessage] = useState("");
+	const form = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			name: "",
+			email: "",
+			message: "",
+		},
+	});
 	const router = useRouter();
 
-	const onSubmit = async (e: FormEvent) => {
-		e.preventDefault();
+	const onSubmit = async (values: z.infer<typeof formSchema>) => {
 		try {
 			const res = await fetch("/api/contact", {
 				method: "POST",
-				body: JSON.stringify({
-					name,
-					email,
-					message,
-				}),
+				body: JSON.stringify(values),
 				headers: {
 					"content-type": "application/json",
 				},
@@ -42,6 +57,7 @@ const ContactForm = ({ slice }: ContactFormProps) => {
 				router.push("/");
 			}
 		} catch (error) {
+			toast.error("Error sending message");
 			console.error("Err", error);
 		}
 	};
@@ -53,45 +69,86 @@ const ContactForm = ({ slice }: ContactFormProps) => {
 		>
 			<div className="rounded-2xl border-2 border-slate-800 bg-slate-900 px-4 py-10 md:px-8 md:py-20">
 				<Heading as="h1">{slice.primary.heading}</Heading>
-				<form
-					onSubmit={onSubmit}
-					className="prose prose-lg prose-invert w-full max-w-none mt-4 md:mt-8"
-				>
-					<div className="flex flex-col md:flex-row md:space-x-4">
-						<input
-							type="text"
-							placeholder="name"
-							value={name}
-							onChange={(e) => setName(e.target.value)}
-							className="prose prose-lg mt-4 w-full h-12 max-w-none rounded-md px-5 bg-background text-foreground"
-						/>
-						<input
-							type="email"
-							placeholder="email"
-							value={email}
-							onChange={(e) => setEmail(e.target.value)}
-							className="prose prose-lg mt-4 w-full h-12 max-w-none rounded-md px-5 bg-background text-foreground"
-						/>
-					</div>
-					<textarea
-						placeholder="message"
-						value={message}
-						onChange={(e) => setMessage(e.target.value)}
-						className="prose prose-lg mt-4 w-full h-40 max-w-none md:mt-10 rounded-md px-5 py-3 bg-background text-foreground"
-					/>
-					<button
-						className="group relative flex w-fit items-center justify-center overflow-hidden rounded-md border-2 border-slate-900 bg-slate-50 px-4 py-2 font-bold text-slate-800 transition-transform ease-out hover:scale-105 mt-8"
-						type="submit"
+				<Form {...form}>
+					<form
+						onSubmit={form.handleSubmit(onSubmit)}
+						className="prose prose-lg prose-invert w-full max-w-none mt-4 md:mt-8"
 					>
-						<span className="absolute inset-0 z-0 h-full translate-y-10 bg-yellow-300 transition-transform duration-300 ease-in-out group-hover:translate-y-0" />
-						<span className="relative flex items-center justify-center gap-2">
-							{slice.primary.button_text}{" "}
-							<MdArrowOutward className="inline-block" />
-						</span>
-					</button>
-				</form>
+						<div className="flex flex-col md:flex-row md:space-x-4">
+							<FormField
+								control={form.control}
+								name="name"
+								render={({ field }) => (
+									<input
+										type="text"
+										placeholder="name"
+										className="prose prose-lg mt-4 w-full h-12 max-w-none rounded-md px-5 bg-background text-foreground"
+										{...field}
+									/>
+								)}
+							/>
+							<FormField
+								control={form.control}
+								name="email"
+								render={({ field }) => (
+									<input
+										type="email"
+										placeholder="email"
+										className="prose prose-lg mt-4 w-full h-12 max-w-none rounded-md px-5 bg-background text-foreground"
+										{...field}
+									/>
+								)}
+							/>
+						</div>
+						<FormField
+							control={form.control}
+							name="message"
+							render={({ field }) => (
+								<textarea
+									placeholder="message"
+									className="prose prose-lg mt-4 w-full h-40 max-w-none md:mt-10 rounded-md px-5 py-3 bg-background text-foreground"
+									{...field}
+								/>
+							)}
+						/>
+						<button
+							className="group relative flex w-fit items-center justify-center overflow-hidden rounded-md border-2 border-slate-900 bg-slate-50 px-4 py-2 font-bold text-slate-800 transition-transform ease-out hover:scale-105 mt-8"
+							type="submit"
+						>
+							<span className="absolute inset-0 z-0 h-full translate-y-10 bg-yellow-300 transition-transform duration-300 ease-in-out group-hover:translate-y-0" />
+							<span className="relative flex items-center justify-center gap-2">
+								{slice.primary.button_text}{" "}
+								<MdArrowOutward className="inline-block" />
+							</span>
+						</button>
+					</form>
+				</Form>
 			</div>
 		</Bounded>
+	);
+};
+
+type FormFieldContextValue<
+	TFieldValues extends FieldValues = FieldValues,
+	TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
+> = {
+	name: TName;
+};
+
+const FormFieldContext = createContext<FormFieldContextValue>(
+	{} as FormFieldContextValue,
+);
+
+const FormField = <
+	TFieldValues extends FieldValues = FieldValues,
+	TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
+>({
+	...props
+}: ControllerProps<TFieldValues, TName>) => {
+	return (
+		<FormFieldContext.Provider value={{ name: props.name }}>
+			<Controller {...props} />
+		</FormFieldContext.Provider>
 	);
 };
 

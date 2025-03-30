3fc069b006a8e6fcc52851c45314bfc724dd721b
THEjacob1000 <jacob@jacobdevelops.com>
2025-03-30 11:33:06 +1100

    chore: update resend package to version 4.2.0, adjust import path for createClient in preview route, and clean up unused dependencies in pnpm-lock.yaml

--- src/app/api/preview/route.ts
+++ src/app/api/preview/route.ts
@@ -1,7 +1,7 @@
 import type { NextRequest } from "next/server";
 import { redirectToPreviewURL } from "@prismicio/next";
 
-import { createClient } from "../../../prismicio";
+import { createClient } from "@/prismicio";
 
 export async function GET(request: NextRequest) {
 	const client = createClient();

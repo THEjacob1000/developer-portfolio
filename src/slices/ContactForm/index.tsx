51550d45a1932f550bdd66145f4bb7edd0a6c600
THEjacob1000 <jacob@jacobdevelops.com>
2025-03-30 12:13:38 +1100

    fix: enhance validation messages in ContactForm schema for better user feedback

--- src/slices/ContactForm/index.tsx
+++ src/slices/ContactForm/index.tsx
@@ -23,9 +23,9 @@ import {
 type ContactFormProps = SliceComponentProps<Content.ContactFormSlice>;
 
 const formSchema = z.object({
-	name: z.string().min(1),
-	email: z.string().email(),
-	message: z.string().min(1),
+	name: z.string().min(1, { message: "Name is required" }),
+	email: z.string().email({ message: "Invalid email address" }),
+	message: z.string().min(1, { message: "Message is required" }),
 });
 
 /**
